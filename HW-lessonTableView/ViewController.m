9eb67e922d95654c31fe576c08e2fcde2bdaf974
Oreo <lala21525@gmail.com>
2015-04-06 10:25:08 +0800

    go

--- HW-lessonTableView/ViewController.m
+++ HW-lessonTableView/ViewController.m
@@ -17,6 +17,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    lessonList = [[NSMutableArray alloc] init];
+    [lessonList addObject:@"View Controller Lifecycle"];
+    [lessonList addObject:@"UIView and other Views"];
+    [lessonList addObject:@"Category & UITableViewController"];
+    [lessonList addObject:@"UICollectionViewController"];
+    [lessonList addObject:@"App Spec Homework"];
+    [lessonList addObject:@"View Controller 總複習"];
+}
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [lessonList count];
+}
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *identifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    cell.textLabel.text = [lessonList objectAtIndex:indexPath.row];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
